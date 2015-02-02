b12f349b49176d389415e3849912f192fb5b4895
Brendan Abbott <brendan@bloodbone.ws>
2015-02-02 20:31:56 +1000

    Fix sorting for entries that don't have any values. RE: #21

--- fields/field.pages.php
+++ fields/field.pages.php
@@ -526,8 +526,8 @@
 	-------------------------------------------------------------------------*/
 
 		public function buildSortingSQL(&$joins, &$where, &$sort, $order='ASC'){
-			$joins .= "INNER JOIN `tbl_entries_data_".$this->get('id')."` AS `ed` ON (`e`.`id` = `ed`.`entry_id`) ";
-			$joins .= "INNER JOIN `tbl_pages` ON (`tbl_pages`.`id` = `ed`.`page_id`) ";
+			$joins .= "LEFT OUTER JOIN `tbl_entries_data_".$this->get('id')."` AS `ed` ON (`e`.`id` = `ed`.`entry_id`) ";
+			$joins .= "LEFT OUTER JOIN `tbl_pages` ON (`tbl_pages`.`id` = `ed`.`page_id`) ";
 			$sort  .= "ORDER BY " . (strtolower($order) == 'random' ? 'RAND()' : "`tbl_pages`.`sortorder` $order");
 		}
 
