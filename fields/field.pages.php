2e90748b5aa59ffa85a996af093fa482e0362ddf
Alexandra Nantel <alexandra@deuxhuithuit.com>
2018-06-11 13:53:42 -0400

    Indent require EQFA right

--- fields/field.pages.php
+++ fields/field.pages.php
@@ -3,7 +3,7 @@
 	if(!defined('__IN_SYMPHONY__')) die('<h2>Symphony Error</h2><p>You cannot directly access this file</p>');
 
 	require_once(TOOLKIT . '/class.pagemanager.php');
-require_once(EXTENSIONS . '/pagesfield/lib/class.entryquerypagesadapter.php');
+	require_once(EXTENSIONS . '/pagesfield/lib/class.entryquerypagesadapter.php');
 
 	Class fieldPages extends Field{
 
